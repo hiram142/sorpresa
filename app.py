032d75ed6b42bf35b49150ee3555af6ef8e25b7d
Hiram Garcia <hiram142@outlook.com>
2026-09-11 17:40:21 -0600

    mas pero mas

--- app.py
+++ app.py
@@ -23,11 +23,11 @@ textos_boton = [
 if st.session_state.clics < 5:
     
     # 1. Movimiento vertical drástico 
-    saltos_verticales = random.randint(15, 15)
+    saltos_verticales = random.randint(6, 15)
     st.markdown(f"{'<br>' * saltos_verticales}", unsafe_allow_html=True)
     
     # 2. Movimiento horizontal 
-    columnas = st.columns(15)
+    columnas = st.columns(3)
     columna_elegida = random.choice(columnas)
     
 
